dbe009d4a66e6aa372abc04e9e02354ec3f73c58
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2025-10-29 00:17:48 +0800

    Fix(Footer): adding new aria label for each social media icons and remove flex wrap from div icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,18 @@ export const Footer = () => {
       <div className="h-0.5 bg-white"></div>
       <div className="flex justify-between gap-4">
         <div className="mt-2">
-          <p className="font-bold font-title">Geraldi Nathan - Copyright 2025</p>
+          <p className="font-bold font-title">
+            Geraldi Nathan - Copyright 2025
+          </p>
         </div>
-        <div className="flex flex-wrap gap-3">
-          <a href={socialMediaLinks.github} target="_blank">
+        <div className="flex gap-3">
+          <a href={socialMediaLinks.github} target="_blank" aria-label="Github">
             <i className="ri-github-fill ri-2x cursor-pointer"></i>
           </a>
-          <a href={socialMediaLinks.linkedin} target="_blank">
+          <a href={socialMediaLinks.linkedin} target="_blank" aria-label="Linkedin">
             <i className="ri-linkedin-fill ri-2x cursor-pointer"></i>
           </a>
-          <a href={socialMediaLinks.instagram} target="_blank">
+          <a href={socialMediaLinks.instagram} target="_blank" aria-label="Instagram">
             <i className="ri-instagram-fill ri-2x cursor-pointer"></i>
           </a>
         </div>
