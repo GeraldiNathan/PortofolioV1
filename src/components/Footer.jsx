b50206e40f44eec6bb4912363ec1926a95abd6ce
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2025-11-05 23:49:06 +0800

    fix(Footer): adding sosmed target link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,10 +14,18 @@ export const Footer = () => {
           <a href={socialMediaLinks.github} target="_blank" aria-label="Github">
             <i className="ri-github-fill ri-2x cursor-pointer"></i>
           </a>
-          <a href={socialMediaLinks.linkedin} target="_blank" aria-label="Linkedin">
+          <a
+            href={socialMediaLinks.linkedin}
+            target="_blank"
+            aria-label="Linkedin"
+          >
             <i className="ri-linkedin-fill ri-2x cursor-pointer"></i>
           </a>
-          <a href={socialMediaLinks.instagram} target="_blank" aria-label="Instagram">
+          <a
+            href={socialMediaLinks.instagram}
+            target="_blank"
+            aria-label="Instagram"
+          >
             <i className="ri-instagram-fill ri-2x cursor-pointer"></i>
           </a>
         </div>
