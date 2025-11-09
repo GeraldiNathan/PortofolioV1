18c020cee4f3e32ed0e0d959d1db2f9259718a33
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2025-11-09 17:02:58 +0800

    feat(main): import footer component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Navbar } from "./components/Navbar.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 // Import Remix Icons CSS
 import "remixicon/fonts/remixicon.css";
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")).render(
     <div className="container mx-auto px-4">
       <Navbar />
       <App />
+      <Footer />
     </div>
   </StrictMode>
 );
