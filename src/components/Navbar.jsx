c19990c77399d33289124ebfdb415083b735b8bc
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2025-12-04 09:11:07 +0800

    feat(Navbar): showing icon text for mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,52 +14,52 @@ export const Navbar = () => {
         md:sticky md:top-0 items-center md:gap-10 md:opacity-100 w-full md:w-fit px-4 py-2 md:py-4 rounded-tl-2xl rounded-tr-2xl 
         md:bg-transparent transition-all md:transition-none"
       >
-        <li>
+        <li className="grid">
+          <a href="#Home" aria-label="Home Section">
+            <i className="ri-home-5-line ri-2x cursor-pointer block sm:hidden"></i>
+          </a>
           <a
             href="#Home"
-            className="sm:text-lg text-base font-medium hidden sm:block"
+            className="sm:text-lg text-xs font-medium"
             aria-label="Home Section"
           >
             Home
           </a>
-          <a href="#Home" aria-label="Home Section">
-            <i className="ri-home-5-line ri-2x cursor-pointer block sm:hidden"></i>
-          </a>
         </li>
-        <li>
+        <li className="grid">
+          <a href="#Project" aria-label="Project Section">
+            <i className="ri-drinks-line ri-2x cursor-pointer block sm:hidden"></i>
+          </a>
           <a
             href="#Project"
-            className="sm:text-lg text-base font-medium hidden sm:block"
+            className="sm:text-lg text-xs font-medium"
             aria-label="Project Section"
           >
             Projects
           </a>
-          <a href="#Project" aria-label="Project Section">
-            <i className="ri-drinks-line ri-2x cursor-pointer block sm:hidden"></i>
-          </a>
         </li>
-        <li>
+        <li className="grid">
+          <a href="#Skill" aria-label="Skill Section">
+            <i className="ri-apps-line ri-2x cursor-pointer block sm:hidden"></i>
+          </a>
           <a
             href="#Skill"
-            className="sm:text-lg text-base font-medium hidden sm:block"
+            className="sm:text-lg text-xs font-medium"
             aria-label="Skill Section"
           >
             Skills
           </a>
-          <a href="#Skill" aria-label="Skill Section">
-            <i className="ri-apps-line ri-2x cursor-pointer block sm:hidden"></i>
-          </a>
         </li>
-        <li>
+        <li className="grid">
+          <a href="#Contact" aria-label="Contact Section">
+            <i className="ri-contacts-line ri-2x cursor-pointer block sm:hidden"></i>
+          </a>
           <a
             href="#Contact"
-            className="sm:text-lg text-base font-medium hidden sm:block"
+            className="sm:text-lg text-xs font-medium"
             aria-label="Contact Section"
           >
             Contact
-          </a>
-          <a href="#Contact" aria-label="Contact Section">
-            <i className="ri-contacts-line ri-2x cursor-pointer block sm:hidden"></i>
           </a>
         </li>
       </ul>
