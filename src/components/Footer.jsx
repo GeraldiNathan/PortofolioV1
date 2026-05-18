aedfe5e053f3c285d2f470b9a179e887ea8542b9
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2026-05-18 08:59:30 +0800

    fix(Footer): Remove social media icon and change to privacy policy and ToS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-import { socialMediaLinks } from "../data";
-
 export const Footer = () => {
   return (
     <div className="py-8 my-9">
@@ -10,24 +8,10 @@ export const Footer = () => {
             Geraldi Nathan - Copyright 2026
           </p>
         </div>
-        <div className="flex gap-3">
-          <a href={socialMediaLinks.github} target="_blank" aria-label="Github">
-            <i className="ri-github-fill ri-2x cursor-pointer"></i>
-          </a>
-          <a
-            href={socialMediaLinks.linkedin}
-            target="_blank"
-            aria-label="Linkedin"
-          >
-            <i className="ri-linkedin-fill ri-2x cursor-pointer"></i>
-          </a>
-          <a
-            href={socialMediaLinks.instagram}
-            target="_blank"
-            aria-label="Instagram"
-          >
-            <i className="ri-instagram-fill ri-2x cursor-pointer"></i>
-          </a>
+        <div className="flex gap-3 mt-2">
+          <a className="underline">Term of Service</a>
+          <span className="text-black dark:text-white">•</span>
+          <a className="underline">Privacy Policy</a>
         </div>
       </div>
     </div>
